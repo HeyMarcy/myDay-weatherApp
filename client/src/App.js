4cb500dae9961f49ee7321f95f9c544889e8d4e1
marcy <marcy.montross@gmail.com>
2017-03-28 15:00:50 -0500

    create componenets

--- client/src/App.js
+++ client/src/App.js
@@ -4,45 +4,79 @@ import Clock from './components/clock';
 import './css/sky.css';
 import './css/index.css';
 
+function CurrentLoc(props){
+return(
+  <form className="currentLocation" >
+    <button className="change-loc">{ props.city }, { props.stateName }</button>
+  </form>
+  )
+ CurrentLoc.propTypes = {
+    city: React.PropTypes.string.isRequired,
+    stateName: React.PropTypes.string.isRequired,
+  }
+}
 
+function LocationForm (props){
+  return (
+    <form className="locationForm" >
+      <input type="text" className="city" placeholder="City"/>
+      <input type="text" className="stateName" placeholder="State"/>
+      <button type="submit" className="change-loc">Go</button>
+      <p><small>For the time being, please us locations within the US.</small></p>
+    </form>
+  )
+}
 
+function Date(props){
+  return (
+    <div className="date">{ props.day },&nbsp; { props.month }&nbsp; {props.dayOfMonth }</div>
+  )
+  Date.propTypes = {
+    day: React.PropTypes.string.isRequired,
+    month: React.PropTypes.string.isRequired,
+    dayOfMonth: React.PropTypes.number.isRequired,
+  }
+}
 
-class App extends Component {
-  render() {
+function WidgetTemp(props){
+  return (
+    <div className="widget">
+      <h6>{ props.temp }</h6>
+      <p>{ props.low }<span className="light"> &nbsp;|&nbsp; </span> { props.high }</p>
+    </div>
+  )
+}
+
+function WidgetWind(props){
+  return (
+    <div className="widget">
+      <h6>{ props.windSpeed } <small>mph</small></h6>
+      <p className="wind-dir"> { props.windDir } </p>
+    </div>
+  )
+}
+
+function App (props) {
     return (
       <div className="App">
         <div className="bg sky-gradient-19">
           <div className="container">
-          <form className="currentLocation" onSubmit={this.handleSubmit}>
-            <button className="change-loc">{ this.props.city }, { this.props.stateName }</button>
-          </form>
-            <form className="locationForm" onSubmit={this.handleSubmit} >
-              <input type="text" className="city" placeholder="City"/>
-              <input type="text" className="stateName" placeholder="State"/>
-              <button type="submit" className="change-loc">Go</button>
-              <p><small>For the time being, please us locations within the US.</small></p>
-            </form>
+            <CurrentLoc city={ props.city } stateName={ props.stateName }  />
+            <LocationForm />
 
-            <div className="timeDate">
-              <div className="date">{ this.props.day },&nbsp; { this.props.month }&nbsp; { this.props.dayOfMonth }</div>
+              <Date day={ props.day } month={ props.month } dayOfMonth={ props.dayOfMonth }/>
               <Clock />
-            </div>
+
             <div className="conditions">
-              <div className="widget">
-                <h6>{ this.props.temp }</h6>
-                <p>{ this.props.low }<span className="light"> &nbsp;|&nbsp; </span> { this.props.high }</p>
-              </div>
-              <div className="widget widget-wind">
-                <h6>{ this.props.windSpeed } <small>mph</small></h6>
-                <p className="wind-dir"> { this.props.windDir } </p>
-              </div>
+              <WidgetTemp temp={props.temp} low={ props.low } high={ props.high }/>
+              <WidgetWind windSpeed={props.windSpeed} windDir={ props.windDir } />
             </div>
           </div>
         </div>
       </div>
     );
   }
-}
+
 
 
 App.defaultProps = {
@@ -50,7 +84,7 @@ App.defaultProps = {
   stateName: "IL",
   day: "Wednesday",
   month: "August",
-  dayOfMonth: "9",
+  dayOfMonth: "10",
   temp: "45",
   high: "48",
   low: "32",
--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-App.defaultProps = {
-  city: "Chicago",
-  stateName: "IL",
-  day: "Wednesday",
-  month: "August",
-  dayOfMonth: "9",
-  temp: "45",
-  high: "48",
-  low: "32",
-  windSpeed: "6.2",
-  windDir: "NNE",
-}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
