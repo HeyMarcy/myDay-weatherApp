2e2f63a5a3074a0a15bda36130d097d07ee89dc7
marcy <marcy.montross@gmail.com>
2017-04-23 14:35:26 -0500

    minor, very minor

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -24,20 +24,6 @@ export const fetchDataError = ( error) => ({
     error
 });
 
-// export const FETCH_WEATHER= 'FETCH_WEATHER';
-// export const fetchWeather = (city="miami", stateName="FL") => dispatch => {
-//     const url = `https://api.wunderground.com/api/fbb8fb3eb8e05269/conditions/forecast/q/${stateName}/${city}/.json`;
-//     return fetch(url).then(response => {
-//         if (!response.ok) {
-//             const error = new Error(response.statusText)
-//             error.response = response
-//             throw error;
-//         }
-//         return response.json();
-//     })
-//     .then(data => dispatch(fetchDataSuccess(data)))
-// };
-
 export const FETCH_WEATHER= 'FETCH_WEATHER';
 export const fetchWeather = (lat='36.8007', lon='-121.9473') => dispatch => {
     const url = `https://api.wunderground.com/api/fbb8fb3eb8e05269/conditions/forecast/q/${lat},${lon}/.json`;
