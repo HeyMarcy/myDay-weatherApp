8511966135e1d7f1622ab627242b80f573e5e608
marcy <marcy.montross@gmail.com>
2017-07-21 15:22:08 -0500

    updating

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,35 +3,48 @@ import { connect } from 'react-redux';
 import { Router, Route } from 'react-router';
 import CurrentLoc from './components/CurrentLoc';
 import Date from './components/Date';
-import Clock from './components/clock';
+import Clock from './components/Clock';
 import WidgetWind from './components/WidgetWind';
 import WidgetTemp from './components/WidgetTemp';
-import * as actions from './actions'
+import * as actions from './actions';
 import './css/sky.css';
 import './css/index.css';
 
-
-class App  extends Component {
-  componentDidMount() {
-    this.props.dispatch(actions.fetchWeather());
-  }
-  render() {
-    return (
-      <div className="App">
-        <div className={`bg sky-gradient-${this.props.hour}`}>
-          <div className="container">
-            <CurrentLoc city={ this.props.city } stateName={ this.props.stateName } />
-            <Date day={this.props.day} month={this.props.month} dayOfMonth={this.props.dayOfMonth}/>
-            <Clock />
-            <div className="conditions">
-              <WidgetTemp temp={this.props.temp} low={ this.props.low } high={ this.props.high }/>
-              <WidgetWind windSpeed={this.props.windSpeed} windDir={ this.props.windDir } />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+class App extends Component {
+	componentDidMount() {
+		this.props.dispatch(actions.fetchWeather());
+	}
+	render() {
+		return (
+			<div className="App">
+				<div className={`bg sky-gradient-${this.props.hour}`}>
+					<div className="container">
+						<CurrentLoc
+							city={this.props.city}
+							stateName={this.props.stateName}
+						/>
+						<Date
+							day={this.props.day}
+							month={this.props.month}
+							dayOfMonth={this.props.dayOfMonth}
+						/>
+						<Clock />
+						<div className="conditions">
+							<WidgetTemp
+								temp={this.props.temp}
+								low={this.props.low}
+								high={this.props.high}
+							/>
+							<WidgetWind
+								windSpeed={this.props.windSpeed}
+								windDir={this.props.windDir}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 // F : focused
@@ -41,22 +54,20 @@ class App  extends Component {
 // T: testable
 //
 
-
 const mapStateToProps = (state, props) => ({
-    changeLoc: state.changeLoc,
-    day: state.day,
-    month: state.month,
-    hour: state.hour,
-    minutes: state.minutes,
-    dayOfMonth: state.dayOfMonth,
-    city: state.city,
-    stateName: state.stateName,
-    temp:state.temp,
-    low:state.low,
-    high: state.high,
-    windSpeed: state.windSpeed,
-    windDir: state.windDir,
-
+	changeLoc: state.changeLoc,
+	day: state.day,
+	month: state.month,
+	hour: state.hour,
+	minutes: state.minutes,
+	dayOfMonth: state.dayOfMonth,
+	city: state.city,
+	stateName: state.stateName,
+	temp: state.temp,
+	low: state.low,
+	high: state.high,
+	windSpeed: state.windSpeed,
+	windDir: state.windDir
 });
 
 export default connect(mapStateToProps)(App);
